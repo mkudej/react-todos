46a14414bedfc7db0368f6f3263714d2a18212ec
mateusz <supermix01@gmail.com>
2021-03-13 18:47:05 +0100

    Added navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="todos-app">
       <Header />
+      <Navigation />
     </div>
   );
 }
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+const StyledNavigation = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  border-bottom: 1px solid #bdbdbd;
+`;
+
+const StyledNavigationButton = styled.button`
+  position: relative;
+  padding: 18px 35px;
+
+  background: none;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 600;
+
+  &.active:after {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 4px;
+
+    background: #2f80ed;
+    border-radius: 4px 4px 0px 0px;
+
+    content: "";
+  }
+`;
+
+const Navigation = () => {
+  return (
+    <StyledNavigation>
+      <StyledNavigationButton className="active">All</StyledNavigationButton>
+      <StyledNavigationButton>Active</StyledNavigationButton>
+      <StyledNavigationButton>Completed</StyledNavigationButton>
+    </StyledNavigation>
+  );
+};
+
+export default Navigation;
