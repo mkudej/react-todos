ade75e2b1abc6b449248a9938a94bfe448ff8fcf
mateusz <supermix01@gmail.com>
2021-03-14 03:24:04 +0100

    Added methods to hande TodoService in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
+import { stat } from "fs";
 import { useState } from "react";
 import AddTodoForm from "./components/AddTodoForm/AddTodoForm";
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 import TodosList from "./components/TodosList/TodosList";
 import { Todo } from "./interfaces/Todo";
+import { TodoService } from "./services/Todo.service";
 
-function App() {
+interface AppProps {
+  todoService: TodoService;
+}
+
+function App({ todoService }: AppProps) {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodo = (title: string) => {
-    setTodos([
-      ...todos,
-      {
-        id: new Date().getTime(),
-        title,
-        completed: false,
-      },
-    ]);
+    todoService.add(title);
+    setTodos(todoService.get());
+  };
+
+  const deleteTodo = (id: number) => {
+    todoService.delete(id);
+    setTodos(todoService.get());
+  };
+
+  const toggleTodoStatus = (id: number) => {
+    todoService.toggleTodoStatus(id);
+    setTodos(todoService.get());
   };
 
   return (
@@ -24,7 +34,11 @@ function App() {
       <Header>#todo</Header>
       <Navigation />
       <AddTodoForm submitTodoForm={addTodo} />
-      <TodosList todos={todos} />
+      <TodosList
+        deleteTodo={deleteTodo}
+        toggleTodoStatus={toggleTodoStatus}
+        todos={todos}
+      />
     </div>
   );
 }
