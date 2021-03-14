7c0f8d8c21d6a64f24f782d12f860463e2cd4804
mateusz <supermix01@gmail.com>
2021-03-14 02:43:32 +0100

    Handling state and submit of AddTodoForm

--- src/components/AddTodoForm/AddTodoForm.tsx
+++ src/components/AddTodoForm/AddTodoForm.tsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import {
   StyledAddTodoForm,
   StyledFormInput,
   StyledFormSubmitButton,
 } from "./styles";
 
-const AddTodoForm = () => {
+interface AddTodoFormProps {
+  submitTodoForm: (title: string) => void;
+}
+
+const AddTodoForm = ({ submitTodoForm }: AddTodoFormProps) => {
+  const [title, setTitle] = useState("");
+
+  const handleTodoTitleChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setTitle(event.target.value);
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (title) {
+      submitTodoForm(title);
+      setTitle("");
+    }
+  };
+
   return (
-    <StyledAddTodoForm>
-      <StyledFormInput placeholder="add details" type="text" />
+    <StyledAddTodoForm onSubmit={handleFormSubmit}>
+      <StyledFormInput
+        onChange={handleTodoTitleChange}
+        value={title}
+        placeholder="add details"
+        type="text"
+      />
       <StyledFormSubmitButton>Add</StyledFormSubmitButton>
     </StyledAddTodoForm>
   );
