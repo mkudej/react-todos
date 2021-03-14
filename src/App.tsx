bdc07e2073b285758f413938ffc3667a4f3ed42e
mateusz <supermix01@gmail.com>
2021-03-14 02:43:00 +0100

    Added Todos state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import AddTodoForm from "./components/AddTodoForm/AddTodoForm";
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 import TodosList from "./components/TodosList/TodosList";
+import { Todo } from "./interfaces/Todo";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (title: string) => {
+    setTodos([
+      ...todos,
+      {
+        id: new Date().getTime(),
+        title,
+        completed: false,
+      },
+    ]);
+  };
+
   return (
     <div className="todos-app">
       <Header>#todo</Header>
       <Navigation />
-      <AddTodoForm />
-      <TodosList />
+      <AddTodoForm submitTodoForm={addTodo} />
+      <TodosList todos={todos} />
     </div>
   );
 }
